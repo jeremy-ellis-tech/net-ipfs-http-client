4c9bbadab29ef8cd4f95909e25a3d9add0760d5c
TrekDev <TrekDev0@gmail.com>
2016-03-22 14:11:10 +0000

    Indent json formatting

--- src/Ipfs/Json/JsonSerializer.cs
+++ src/Ipfs/Json/JsonSerializer.cs
@@ -4,14 +4,24 @@ namespace Ipfs.Json
 {
     public class JsonSerializer : IJsonSerializer
     {
+        private readonly JsonSerializerSettings _settings;
+
+        public JsonSerializer()
+        {
+            _settings = new JsonSerializerSettings
+            {
+                Formatting = Formatting.Indented
+            };
+        }
+
         public T Deserialize<T>(string json)
         {
-            return JsonConvert.DeserializeObject<T>(json);
+            return JsonConvert.DeserializeObject<T>(json, _settings);
         }
 
         public string Serialize(object obj)
         {
-            return JsonConvert.SerializeObject(obj);
+            return JsonConvert.SerializeObject(obj, _settings);
         }
     }
 }
