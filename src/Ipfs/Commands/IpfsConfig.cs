ca93fc0c519d6a505520dc0ecb77f3c0b8b48ad8
Timothy Parez <timothyparez@gmail.com>
2017-06-14 14:33:15 +0800

    Added cancellation support to the IpfsConfig class
    Added cancellation support to the methods that end up calling ExecuteGetAsync and ExecutePostAsync

--- src/Ipfs/Commands/IpfsConfig.cs
+++ src/Ipfs/Commands/IpfsConfig.cs
@@ -1,6 +1,7 @@
 ﻿using Ipfs.Json;
 using System;
 using System.Net.Http;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Ipfs.Commands
@@ -16,10 +17,11 @@ namespace Ipfs.Commands
         /// To use 'ipfs config edit', you must have the $EDITOR environment
         /// variable set to your preferred text editor.
         /// </summary>
+        /// <param name="cancellationToken">Token allowing you to cancel the request</param>
         /// <returns></returns>
-        public async Task<HttpContent> Edit()
+        public async Task<HttpContent> Edit(CancellationToken cancellationToken = default(CancellationToken))
         {
-            return await ExecuteGetAsync("edit");
+            return await ExecuteGetAsync("edit", cancellationToken);
         }
 
         /// <summary>
@@ -28,10 +30,11 @@ namespace Ipfs.Commands
         /// can't be undone.
         /// </summary>
         /// <param name="file">The file to use as the new config</param>
+        /// <param name="cancellationToken">Token allowing you to cancel the request</param>
         /// <returns></returns>
-        public async Task<HttpContent> Replace(string file)
+        public async Task<HttpContent> Replace(string file, CancellationToken cancellationToken = default(CancellationToken))
         {
-            return await ExecuteGetAsync("replace", file);
+            return await ExecuteGetAsync("replace", file, cancellationToken);
         }
 
         /// <summary>
@@ -39,10 +42,11 @@ namespace Ipfs.Commands
         /// WARNING: Your private key is stored in the config file, and it will be
         /// included in the output of this command.
         /// </summary>
+        /// <param name="cancellationToken">Token allowing you to cancel the request</param>
         /// <returns></returns>
-        public async Task<IpfsConfigShow> Show()
+        public async Task<IpfsConfigShow> Show(CancellationToken cancellationToken = default(CancellationToken))
         {
-            HttpContent content = await ExecuteGetAsync("show");
+            HttpContent content = await ExecuteGetAsync("show", cancellationToken);
             var json = await content.ReadAsStringAsync();
             return _jsonSerializer.Deserialize<IpfsConfigShow>(json);
         }
